a1e540af31d55563f20582ac939ef16c5c973344
System Administrator <admin@example.com>
2019-11-21 15:00:45 -0500

    Changes by 'System Administrator' on 'Demo TAG - 1.0.0' -- Build #1

---
+++ sn/Demo TAG/Dictionary Entry/x_308211_demo_tag_table/active_true/number/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo TAG
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-11-21 19:50:43
+ * Created By  : admin
+ * Updated On  : 2019-11-21 19:50:43
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=5a29e0c5dbd14010343f58b3ca961944
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
